cb518118d0b190e6bcfb2be2c964642c4b06d5a6
Andy Mai <andymai@rescomp-13-213410.stanford.edu>
2014-01-28 00:30:56 -0800

    initial commit

---
+++ pickup/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'pickup.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'pickupApp.views.index', name='index'),
+    url(r'^register/', 'pickupApp.views.register', name='register'),
+    url(r'^home/', 'pickupApp.views.home', name='home'),
+    url(r'^login/', 'pickupApp.views.user_login', name='user_login'),
+)
