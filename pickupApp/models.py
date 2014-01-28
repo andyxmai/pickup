cb278d1554fe34e1d2625267ab77a38a596f9515
Ethan Myers <ethan.myers@gmail.com>
2014-01-28 01:27:38 -0800

    Added game table with user fields

--- pickupApp/models.py
+++ pickupApp/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-	first_name = models.CharField(max_length=200, null=True)
-	last_name = models.CharField(max_length=200, null=True)
-	email = models.EmailField()
-	password = models.CharField(max_length=50)
 
-class Games(models.Model):
+class Game(models.Model):
 	sport = models.CharField(max_length=50, null=True)
+	name = models.CharField(max_length=50)
+	description = models.CharField(max_length=400)
+	latitude = models.FloatField()
+	longitude = models.FloatField()
+	users = models.ManyToManyField(User)
+
 	
